858fb12516202dbffef9d77c98e325665b3af69e
Martin <mail@mama.sh>
2020-11-01 21:29:01 +0100

    Animations on buttons

--- java/sh/mama/hangman/activities/MainActivity.kt
+++ java/sh/mama/hangman/activities/MainActivity.kt
@@ -2,36 +2,43 @@ package sh.mama.hangman.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import sh.mama.hangman.R
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    private val buttonClick = AlphaAnimation(1F, 0.1F)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         play.setOnClickListener {
+            it.startAnimation(buttonClick)
             val game = Intent(this, PickCategoryActivity::class.java)
             game.putExtra("edit", false)
             startActivity(game)
         }
         create_categories.setOnClickListener {
+            it.startAnimation(buttonClick)
             val create = Intent(this, PickCategoryActivity::class.java)
             create.putExtra("edit", true)
             startActivity(create)
         }
         watch_highscores.setOnClickListener {
+            it.startAnimation(buttonClick)
             val watchHighScores = Intent(this, HighScoreActivity::class.java)
             startActivity(watchHighScores)
         }
         add_word_button.setOnClickListener {
+            it.startAnimation(buttonClick)
             val addWord = Intent(this, EditWordsActivity::class.java)
             addWord.putExtra("create", true)
             startActivity(addWord)
         }
         exit.setOnClickListener {
+            it.startAnimation(buttonClick)
             end()
         }
     }
