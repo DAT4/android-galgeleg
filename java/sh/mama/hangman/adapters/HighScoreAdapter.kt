810e510078022673de72cd66c349b610a8c4b517
Martin <mail@mama.sh>
2020-11-02 16:03:15 +0100

    Recyclerview til highscores

---
+++ java/sh/mama/hangman/adapters/HighScoreAdapter.kt
@@ -0,0 +1,38 @@
+package sh.mama.hangman.adapters
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_category.view.*
+import sh.mama.hangman.activities.PlayGameActivity
+import sh.mama.hangman.R
+import sh.mama.hangman.activities.EditWordsActivity
+import sh.mama.hangman.models.HighScore
+import sh.mama.hangman.models.Word
+
+class HighScoreAdapter(
+    private var scores: List<HighScore>,
+) : RecyclerView.Adapter<HighScoreAdapter.HighScoreViewHolder>() {
+    inner class HighScoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private val buttonClick = AlphaAnimation(1F, 0.8F)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HighScoreViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_category, parent, false)
+        return HighScoreViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HighScoreViewHolder, position: Int) {
+        holder.itemView.apply {
+            catTitle.text = scores[position].player
+            catCount.text = scores[position].getScore().toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return scores.size
+    }
+}
