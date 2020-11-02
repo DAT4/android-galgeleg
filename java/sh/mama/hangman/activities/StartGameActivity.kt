038b1ddd41a9cbfa4ac13b3c0264ae556050c4b0
Martin <mail@mama.sh>
2020-11-02 21:27:18 +0100

    Validation on word

--- java/sh/mama/hangman/activities/StartGameActivity.kt
+++ java/sh/mama/hangman/activities/StartGameActivity.kt
@@ -3,12 +3,11 @@ package sh.mama.hangman.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AlphaAnimation
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_start_game.*
 import sh.mama.hangman.R
 import sh.mama.hangman.models.Word
-import kotlin.system.exitProcess
 
 class StartGameActivity : AppCompatActivity() {
     private val buttonClick = AlphaAnimation(1F, 0.1F)
@@ -20,14 +19,17 @@ class StartGameActivity : AppCompatActivity() {
 
         start_game.setOnClickListener {
             it.startAnimation(buttonClick)
-            var name = player_name.text.toString()
+            var name = ""
+            name = player_name.text.toString()
             if (name == "")
-                name = "Unknown"
-            val intent = Intent(this,PlayGameActivity::class.java)
-            intent.putExtra("name",name)
-            intent.putExtra("word",word)
-            startActivity(intent)
-            finish()
+                Toast.makeText(this, "You have to pick a name", Toast.LENGTH_SHORT).show()
+            else {
+                val intent = Intent(this, PlayGameActivity::class.java)
+                intent.putExtra("name", name)
+                intent.putExtra("word", word)
+                startActivity(intent)
+                finish()
+            }
         }
 
     }
