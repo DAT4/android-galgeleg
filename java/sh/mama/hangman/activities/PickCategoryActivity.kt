d1df7eb1bfcf37a727e1c8b1acf660f43ab3cb62
Martin <mail@mama.sh>
2020-11-02 22:42:09 +0100

    refactor reformat

--- java/sh/mama/hangman/activities/PickCategoryActivity.kt
+++ java/sh/mama/hangman/activities/PickCategoryActivity.kt
@@ -1,27 +1,26 @@
 package sh.mama.hangman.activities
 
 import android.os.Bundle
-import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_pick_context.*
+import sh.mama.hangman.Observer.ConcreteWords
 import sh.mama.hangman.Observer.IObserver
 import sh.mama.hangman.R
+import sh.mama.hangman.adapters.ActionType
 import sh.mama.hangman.adapters.CategoryAdapter
 import sh.mama.hangman.libs.DataGetter.getWords
 import sh.mama.hangman.models.Category
-import sh.mama.hangman.Observer.ConcreteWords
-import sh.mama.hangman.adapters.ActionType
 
 class PickCategoryActivity : AppCompatActivity(), IObserver {
-    private lateinit var actionType:ActionType
+    private lateinit var actionType: ActionType
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_context)
         this.actionType = intent.getSerializableExtra("actionType") as ActionType
         ConcreteWords.add(this)
-        if (ConcreteWords.isNull()){
+        if (ConcreteWords.isNull()) {
             getWords()
         } else {
             printButtons(ConcreteWords.getCategories())
